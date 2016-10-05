f987d9f6d7e09773998cc913b3dcce703fa65b0c
DylanMaugee <maugee.dylan@gmail.com>
2016-10-05 14:32:23 +0200

    Add files via upload

--- page/inscription.php
+++ page/inscription.php
@@ -1,4 +1,30 @@
 <?php
 include_once('../include/fonctions.php');
-$db= new App\fonctions();
-$db->inscription();
+$db = new App\fonctions();
+
+if (isset($_POST['inscription'])) {
+    try {
+        $db->inscription($_POST['username'], $_POST['password']);
+        header('Location: connexion.php');
+    } catch (Exception $ex) {
+        echo 'Inscription échouée';
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Forum | Inscription</title>
+    </head>
+    <body>
+        <!--Creation du formulaire-->
+        <form action="inscription.php" method="POST">
+            <label for="username">Username : </label>
+            <input name="username" type="text" required/><br/><br/>
+            <label for="password">Password : </label>
+            <input name="password" type="password" required/><br/><br/>
+            <input type="Submit" name="inscription" value="Inscription"/>
+        </form>
+    </body>
+</html>
