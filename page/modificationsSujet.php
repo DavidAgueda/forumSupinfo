cb6ad620e855a5a113fb51c57754173d6f1c4ab5
David <David@LAPTOP-41ONIHLN>
2016-10-05 14:37:52 +0200

    add modificationsSujet.php

--- page/modificationsSujet.php
+++ page/modificationsSujet.php
@@ -1,8 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ include_once('../include/fonctions.php');
+ $db = new App\fonctions();
+ 
+ if (isset($_POST['inscription'])) {
+     try {
+         $db->inscription($_POST['username'], $_POST['password']);
+         header('Location: connexion.php');
+     } catch (Exception $ex) {
+         echo 'Inscription échouée';
+     }
+ }
+ ?>
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <meta charset="UTF-8">
+         <title>Forum | Inscription</title>
+     </head>
+     <body>
+         <!--Creation du formulaire-->
+         <form action="inscription.php" method="POST">
+             <label for="username">Username : </label>
+             <input name="username" type="text" required/><br/><br/>
+             <label for="password">Password : </label>
+             <input name="password" type="password" required/><br/><br/>
+             <input type="Submit" name="inscription" value="Inscription"/>
+         </form>
+     </body>
+ </html> 
